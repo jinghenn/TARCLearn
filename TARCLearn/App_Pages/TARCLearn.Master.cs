53337b747116c91ff4de0ea4e061ba6781854da3
zi yan <yokipoh11@gmail.com>
2021-07-20 21:37:06 +0800

    add discussion

--- TARCLearn/App_Pages/TARCLearn.Master.cs
+++ TARCLearn/App_Pages/TARCLearn.Master.cs
@@ -11,6 +11,19 @@ namespace TARCLearn.App_Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string userId = Session["userId"].ToString();
+            if (userId == null)
+            {
+                System.Text.StringBuilder javaScript = new System.Text.StringBuilder();
+                string scriptKey = "ErrorMessage";
+
+                javaScript.Append("var userConfirmation = window.confirm('" + "Your Session has Expired, Please login again.');\n");
+                javaScript.Append("window.location='Login.aspx';");
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), scriptKey, javaScript.ToString(), true);
+                
+            }
+
             if (Session["username"] != null)
             {
                 lblUserName.Text = Session["username"].ToString();
